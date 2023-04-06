c00765b413f29ab5496279021791a474f9aa968c
Mat_Fisher <matvey@rybakov.pro>
2023-04-06 13:08:15 +0300

    Добавлен метод решения

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿string[] arrayStart = new string[5] {"123", "23", "hello", "world", "res"};
 string[] arrayResult = new string[array1.Length];
+
+string[] SecondArrayWithIF(string[] arrayStart, string[] arrayResult)
+{
+    int temp = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+    if(arrayStart[i].Length <= 3)
+        {
+        arrayResult[temp] = arrayStart[i];
+        temp++;
+        }
+    }
+    return arrayResult;
+}
+
